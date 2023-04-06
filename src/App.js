1f1401ce77da580f8918c465cbfb0ccae558836c
João Marcus <joaomarcus75@gmail.com>
2023-04-06 11:45:05 -0300

    First Implementations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import React from 'react';
+import React ,{useState} from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='App'>
+    <MyComponent/>
+   </div>
+  );
+}
+
+function MyComponent()
+{
+  const [text, setText] = useState('Hello World!');
+  const handleClick = () => {
+    setText('The text was changed!');
+  }
+  return(
+    <div>
+      <h1>{text}</h1>
+      <button onClick={handleClick}>Clique Aqui</button>
     </div>
   );
 }
 
 export default App;
+  
